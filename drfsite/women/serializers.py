2f5cf8ce8e65fbc6c19cda6523bb800b35827fb1
Aleksey Strelkov <aleks.strel.8v@gmail.com>
2022-03-31 11:49:32 +0300

    v.4: add: Serializer class
    Added class for data serializer with get and post methods.

--- drfsite/women/serializers.py
+++ drfsite/women/serializers.py
@@ -1,11 +1,61 @@
+import io
+
 from rest_framework import serializers
 from .models import Women
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
 
 
-class WomenSerializer(serializers.ModelSerializer):
-    '''Наследуется от серелизатора, который работает с моделями'''
-    class Meta:
-        model = Women
-        # Ниже приведены поля для серилизации, то есть те, 
-        # котрые будут отправляться обратно пользователяю
-        fields = ('title', 'cat_id')
+# class WomenModel:
+
+#     '''Объекты этого класса мы будем сериализовать, то есть преобразовывать в json строку.
+#     Этот класс будет как бы имитировать модели фреймворка Django'''
+
+#     def __init__(self, title, content):
+#         self.title = title
+#         self.content = content
+
+
+class WomenSerializer(serializers.Serializer):
+
+    '''Класс-сериализатор для модели women. С помощью которого преобразуем объекты моделей в словарь,
+    а затем его в json строку с помощью json render'''
+
+    title = serializers.CharField(max_length=255)
+    content = serializers.CharField()
+    time_create = serializers.DateTimeField(read_only=True)  # Параметры только для чтения
+    time_update = serializers.DateTimeField(read_only=True)  # Параметры только для чтения
+    is_published = serializers.BooleanField(default=True)
+    cat_id = serializers.IntegerField()
+
+
+# class WomenSerializer(serializers.Serializer):
+
+#     '''Наследуется от серелизатора, который работает с моделями'''
+#     class Meta:
+
+#         model = Women
+#         # Ниже приведены поля для серилизации, то есть те,
+#         # котрые будут отправляться обратно пользователяю
+#         fields = ('title', 'cat_id')
+
+
+# # Функция кодирует данные в JSON -> заменены на класс
+# def encode():
+#     '''Прогоняем объект model через сериализитор. То коллеция data представляет собой словарь.
+#     Затем его пропускаем через json render, и получаем байтовую строку. Которую можно отдавать клиенту.
+#     То есть объект -> словарь -> json-строка.'''
+#     model = WomenModel('Angelina Jolie', 'Content: Angelina Jolie')
+#     model_sr = WomenSerializer(model)  # Здесь отрабатывает сериализатор вместо атрибутов создаёт коллецию data
+#     print(model_sr.data, type(model_sr.data), sep='\n')
+#     json = JSONRenderer().render(model_sr.data)  # Преобразует объект серилизации в байтовую JSON строку
+#     print(json, type(json), sep='\n')
+
+
+# # Функция декодирует данные из JSON -> заменены на класс
+# def decode():
+#     stream = io.BytesIO(b'{"title":"Angelina Jolie","content":"Content: Angelina Jolie"}')
+#     data = JSONParser().parse(stream)  # Передаём весь поток парсеру
+#     serializer = WomenSerializer(data=data)  # Преобразовываем, чтобы получить объект данных
+#     serializer.is_valid()  # Проверка принятых данных на корректность
+#     print(serializer.validated_data)
--- drfsite/women/views.py
+++ drfsite/women/views.py
@@ -6,29 +6,52 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 
-# class WomenAPIView(generics.ListAPIView):
-#     queryset = Women.objects.all()
-#     serializer_class = WomenSerializer
+# class WomenAPIView(APIView):
+
+#     '''APIView стоит во главе всех классов предствления API в рамках Django REST Framework'''
+
+#     # Этот метод обрабатывает GET запросы поступающие на сервер
+#     def get(self, request):
+#         # Формирование строки JSON
+#         # return Response({'title': 'Angelina Jolie'})
+#         # Здесь выбираем все записи из таблицы women, преобразуем их к списку и возвращаем в виде JSON-строки
+#         lst = Women.objects.all().values()
+#         return Response({'posts': list(lst)})
+
+#     # Этот метод обрабатывает POST запросы
+#     def post(self, request):
+#         post_new = Women.objects.create(
+#             title=request.data['title'],
+#             content=request.data['content'],
+#             cat_id=request.data['cat_id']
+#         )
+
+#         return Response({'post': model_to_dict(post_new)})
 
 
 class WomenAPIView(APIView):
 
-    '''APIView стоит во главе всех классов предствления API в рамках Django REST Framework'''
+    ''''''
 
-    # Этот метод обрабатывает GET запросы поступающие на сервер
     def get(self, request):
-        # Формирование строки JSON
-        # return Response({'title': 'Angelina Jolie'})
-        # Здесь выбираем все записи из таблицы women, преобразуем их к списку и возвращаем в виде JSON-строки
-        lst = Women.objects.all().values()
-        return Response({'posts': list(lst)})
+        # ФОрмируем список объектов класса Women
+        w = Women.objects.all()  # Список статей получаем как набор queryset
+        # Передаём весь список на сериализатор. Response преобразовывает всё в байтовую JSON строку
+        # Response вызывает JSON render и преобразовывает данные
+        return Response({'posts': WomenSerializer(w, many=True).data})
 
-    # Этот метод обрабатывает POST запросы
     def post(self, request):
+        serializer = WomenSerializer(data=request.data)
+        # Проверка корректности принятных данных, если каких-то данных не будет хватать в запросе,
+        # то будет сгенерированно исключение
+        serializer.is_valid(raise_exception=True)
+
+        # Если проверка прошла успешно, то добавляем данные в базу данных
         post_new = Women.objects.create(
             title=request.data['title'],
             content=request.data['content'],
             cat_id=request.data['cat_id']
         )
 
-        return Response({'post': model_to_dict(post_new)})
+        # Возвращаем то, что было добавлено
+        return Response({'post': WomenSerializer(post_new).data})
